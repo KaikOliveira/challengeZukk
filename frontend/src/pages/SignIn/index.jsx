eff0eba8d7271543375ff5313519cce3458c07df
KaikOliveira <69028381+KaikOliveira@users.noreply.github.com>
2021-01-20 21:56:08 -0300

    input and button

--- frontend/src/pages/SignIn/index.jsx
+++ frontend/src/pages/SignIn/index.jsx
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, { useCallback, useRef} from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
-import { FormHandles } from '@unform/core';
 
-import { Wrapper, ConteinerForm, Input, Button } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Wrapper, ConteinerForm} from './styles';
 
 function SignIn() {
+  const formRef = useRef(null);
+
+  const handleSubmit = useCallback(() => {
+    console.log("deu")
+  },[]);
+
   return (
     <Wrapper>
       <ConteinerForm>
-        <h1>Faça o Login</h1>
-        <Input
-          name="email"
-          icon={FiMail}
-          placeholder="E-mail"
-        />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Senha"
-        />
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
 
-        <Button type="submit">Entrar</Button>
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
       </ConteinerForm>
     </Wrapper>
   );
